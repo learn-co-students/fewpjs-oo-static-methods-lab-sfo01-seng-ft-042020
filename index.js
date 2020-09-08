49a7d1a75b81abf3866eaf7d72f1c6a3ce786046
tharveybrown <tharveybrown@gmail.com>
2020-09-08 09:19:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static titleize(strings) {
+    const lowercaseWords = new Set([
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "the",
+      "from",
+    ]);
+    const words = strings.split(" ");
+    let titleized = [];
+    words.forEach((word, index) => {
+      if (lowercaseWords.has(word) && index !== 0) {
+        titleized.push(word);
+      } else {
+        titleized.push(this.capitalize(word));
+      }
+    });
+    return titleized.join(" ");
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, "");
+  }
 }
